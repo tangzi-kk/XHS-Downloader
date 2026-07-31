091bf80941084dc6e91b30519a1d0c0ec400bf87
塘子kk <58504225+tangzi-kk@users.noreply.github.com>
2026-07-31 17:19:36 +0800

    fix: support xhslink.cn short links

--- source/application/__init__.py
+++ source/application/__init__.py
@@ -1,3 +1,11 @@
+from re import compile
+
 from .app import XHS
+
+XHS.SHORT = compile(
+    r"(?:https?://)?(?:www\.)?(?:xhslink|xhsurl)\.(?:cn|com)/"
+    r"(?:(?:o|m)/)?[A-Za-z0-9_-]+"
+    r"(?:\?[A-Za-z0-9._~!$&'()*+,;=:@%/?#\-\[\]]*)?"
+)
 
 __all__ = ["XHS"]
